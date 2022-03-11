23052c8071c46a80a5d64804d396120e2b5f1b3f
Edgar0914 <93760846+Edgar0914@users.noreply.github.com>
2022-03-11 12:18:36 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    navgation: true,
+    navigationToultips: ['1', '2', '3', '4', '5', '6'],
+    showActiveTooltip: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourPage', 'fivePage', 'sixPage'],
+    scrollingSpeed: 1000,
+
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
